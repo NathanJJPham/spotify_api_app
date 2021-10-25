927ab3b869fad4bbae8f45ae07d272b6b1c5418c
NathanJJPham <nathan.pham@kin.com>
2021-10-25 13:41:22 -0400

    Revert "artists"
    This reverts commit ee3330a4acab773a03a56d272d31f45340d247fa.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  # get 'artists/index'
-  # get 'artists/show'
-  # get 'artists/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/artists', to: 'artists#index'
-get '/artists/:id', to: 'artists#show', as: 'artist'
-get '/search', to: 'artists#search'
 end
